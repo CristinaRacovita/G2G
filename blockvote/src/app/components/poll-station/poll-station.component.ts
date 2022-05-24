3f7c8df7445cbc3b12cea5041572ca144305f696
Cristina Racovita <cristina.racovita98@gmail.com>
2022-05-24 23:57:24 +0300

    implement poll station page

--- blockvote/src/app/components/poll-station/poll-station.component.ts
+++ blockvote/src/app/components/poll-station/poll-station.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-poll-station',
-  templateUrl: './poll-station.component.html',
-  styleUrls: ['./poll-station.component.scss']
+  selector: "app-poll-station",
+  templateUrl: "./poll-station.component.html",
+  styleUrls: ["./poll-station.component.scss"],
 })
-export class PollStationComponent implements OnInit {
+export class PollStationComponent {
+  public choosenOption = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public chooseOption(option: string): void {
+    this.choosenOption = option;
   }
 
+  public  get voted(): string{
+    if(this.choosenOption){
+      return '';
+    }
+
+    return 'disabled';
+  }
+
+  public get isVoted(): boolean{
+    if(this.choosenOption){
+      return true;
+    }
+
+    return false;
+  }
 }
